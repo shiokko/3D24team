48acd0dc4ec6e8be0f6e6d910a90b27c2e554e1c
Zhanzii9624 <sophiajeng88@gmail.com>
2024-11-03 23:57:57 +0800

    Add washing veg related
    -add feature: wash veg

--- 3D24team/Assets/Scripts/WaterController.cs
+++ 3D24team/Assets/Scripts/WaterController.cs
@@ -20,5 +20,24 @@ public class WaterController : MonoBehaviour
         animator.SetBool("is_streaming", false);
     }
 
+    // 碰撞檢測
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        // 檢查碰撞的物件是否具有 raw_vege 腳本
+        raw_vege washable = collider.gameObject.GetComponent<raw_vege>();
+        if (washable != null)
+        {
+            StartCoroutine(WashObjectAfterDelay(washable));  // 啟動協程
+        }
+    }
+
+    // 等待2秒後設置 isWashed 為 true
+    private IEnumerator WashObjectAfterDelay(raw_vege washable)
+    {
+        StartWaterFlow();
+        yield return new WaitForSeconds(2f);  // 等待 2 秒
+        washable.isWashed = true;  // 設置 isWashed 為 true
+        StopWaterFlow();
+    }
 
 }
