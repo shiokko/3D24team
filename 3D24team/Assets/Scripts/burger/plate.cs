6fd1ac672c66a503891b4b6c238fbe1875ecc151
ching11720 <jasmine910402614@gmail.com>
2024-11-03 23:12:20 +0800

    re-organize the file

---
+++ 3D24team/Assets/Scripts/burger/plate.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    public int ingredientCount = 0;
+    public bool isDone = false;
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Vector3 originalPosition;
+    public GameObject prefab_raw;
+    public GameObject plateTopObject;
+    private plateTop plateTop;
+
+    void Start()
+    {
+        plateTop = plateTopObject.GetComponent<plateTop>();
+        ingredientCount = 0;
+        isDone = false;
+        plateTop.ResetSprite();
+    }
+    
+    void OnMouseDown()
+    {
+        if (isDone)
+        {
+            isDragging = true;
+            Instantiate(prefab_raw, transform.position, Quaternion.identity);
+            offset = transform.position - GetMouseWorldPosition();
+            originalPosition = transform.position;
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            transform.position = GetMouseWorldPosition() + offset;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("bread") && ingredientCount == 0)
+        {
+            plateTop.ChangeSprite(ingredientCount);
+            ingredientCount++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("cut_lettuce") && ingredientCount == 1)
+        {
+            plateTop.ChangeSprite(ingredientCount);
+            ingredientCount++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("friedBeef") && ingredientCount == 2)
+        {
+            plateTop.ChangeSprite(ingredientCount);
+            ingredientCount++;
+            gameObject.tag = "BeefBurger";
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("friedPork") && ingredientCount == 2)
+        {
+            plateTop.ChangeSprite(7);
+            ingredientCount++;
+            gameObject.tag = "PorkBurger";
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("cut_tomato") && ingredientCount == 3)
+        {
+            plateTop.ChangeSprite(ingredientCount);
+            ingredientCount++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("cut_cucumber") && ingredientCount == 4)
+        {
+            plateTop.ChangeSprite(ingredientCount);
+            ingredientCount++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("friedEgg") && ingredientCount == 5)
+        {
+            plateTop.ChangeSprite(ingredientCount);
+            ingredientCount++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("bread") && ingredientCount == 6)
+        {
+            plateTop.ChangeSprite(ingredientCount);
+            Destroy(collision.gameObject);
+            isDone = true;
+        }
+    }
+}
