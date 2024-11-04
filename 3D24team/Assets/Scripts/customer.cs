a8ca746b622ec87482cd62bf26344fddfc9ff99f
ching11720 <jasmine910402614@gmail.com>
2024-11-04 18:01:12 +0800

    comlete the scene

--- 3D24team/Assets/Scripts/customer.cs
+++ 3D24team/Assets/Scripts/customer.cs
@@ -1,24 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class customer : MonoBehaviour
 {
     public string orderedItem;
-    private gameManager gameManager; 
+    public TextMeshProUGUI orderText;
+    public Image orderImage;
+    public Button rejectButton;
 
     void Start()
     {
-        gameManager = FindObjectOfType<gameManager>(); 
+        orderText.gameObject.SetActive(true);
+        rejectButton.gameObject.SetActive(true);
+        orderImage.gameObject.SetActive(true);
+    }
+
+    public void SetOrder(string newOrder, int SP)
+    {
+        orderedItem = newOrder;
+        RectTransform orderTextRect = orderText.GetComponent<RectTransform>();
+        RectTransform rejectButtonRect = rejectButton.GetComponent<RectTransform>();
+        RectTransform orderImageRect = orderImage.GetComponent<RectTransform>();
+
+        Debug.Log("SP: " + SP);
+        if (SP == 0)
+        {
+            orderTextRect.anchoredPosition = new Vector3(-231.8f, 193.49f, 0);
+            rejectButtonRect.anchoredPosition = new Vector3(-231.8f, 162.55f, 0);
+            orderImageRect.anchoredPosition = new Vector3(-231.8f, 193.49f, 0);
+            Debug.Log("txt1: " + orderTextRect.anchoredPosition);
+        }
+
+        if (SP == 1)
+        {
+            orderTextRect.anchoredPosition = new Vector3(57.3f, 193.49f, 0);
+            rejectButtonRect.anchoredPosition = new Vector3(57.3f, 162.55f, 0);
+            orderImageRect.anchoredPosition = new Vector3(57.3f, 193.49f, 0);
+            Debug.Log("txt2: " + orderTextRect.anchoredPosition);
+        }
+
+        orderText.text = "I want \n" + orderedItem + "!";
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Customer collided with " + other.tag + ", and order is " + orderedItem);
         if (other.CompareTag(orderedItem))
         {
-            gameManager.UpdateMoney(orderedItem);
-            Destroy(gameObject);
+            Serve();
             Destroy(other.gameObject);
         }
+    }
+
+    public void Serve()
+    {
+        FindObjectOfType<gameManager>().CustomerServed();
+        HideUI();
+        Destroy(gameObject);
+    }
+
+    public void Reject()
+    {
+        FindObjectOfType<gameManager>().CustomerRejected();
+        HideUI();
+        Destroy(gameObject);
+    }
+
+    private void HideUI()
+    {
+        orderText.gameObject.SetActive(false);
+        rejectButton.gameObject.SetActive(false);
+        orderImage.gameObject.SetActive(false);
     }
 }
--- 3D24team/Assets/Scripts/gameManager.cs
+++ 3D24team/Assets/Scripts/gameManager.cs
@@ -2,69 +2,129 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class gameManager : MonoBehaviour
 {
-    public Text moneyText;
-    public int money = 0;
-    public int totalCustomers = 10;
+    public float timer = 180f;
+    private float timeLeft;
+    public TextMeshProUGUI timerText;
+    public GameObject gameOverText;
+
     public GameObject customerPrefab;
-    public Transform spawnPoint;
     private List<string> menuItems = new List<string>();
+    public Transform[] spawnPoints;
+    private string orderedItem = "";
+
+    public TextMeshProUGUI moneyText;
+    public int money = 0;
+
+    void Start()
+    {
+        // Debug.Log("Start() called.");
+        timeLeft = timer;
+        timerText.text = "Time Left: " + Mathf.Round(timeLeft) + "s";
+        gameOverText.SetActive(false);
+        menuItems = new List<string> { "BeefBurger", "PorkBurger", "salad"};
+        StartCoroutine(SpawnCustomers());
+        // UpdateMoneyText();
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timerText.text = "Time Left: " + Mathf.Round(timeLeft) + "s";
+
+        if (timeLeft <= 0)
+        {
+            timerText.text = "Time Left: " + Mathf.Round(timeLeft);
+            gameOverText.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    private int GetAvailableSpawnPointIndex()
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (spawnPoints[i].childCount == 0) 
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private IEnumerator SpawnCustomers()
+    {
+        // Debug.Log("SpawnCustomers() called.");
+        while (true)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                int SP = Random.Range(0, spawnPoints.Length);
+                Transform spawnPoint = spawnPoints[SP];
+
+                if (spawnPoint.childCount > 0)
+                {
+                    continue;
+                }
+
+                SpawnCustomer(spawnPoint, SP);
+            }
+            yield return new WaitForSeconds(10f);
+        }
+    }
+
+    private void SpawnCustomer(Transform spawnPoint, int SP)
+    {
+        GameObject newCustomer = Instantiate(customerPrefab, spawnPoint.position, Quaternion.identity);
+        newCustomer.transform.SetParent(spawnPoint);
+        customer customerScript = newCustomer.GetComponent<customer>();
+        
+        orderedItem = menuItems[Random.Range(0, menuItems.Count)];
+        // Debug.Log("SP: " + SP);
+        // Debug.Log("randMenuItem: " + orderedItem + " and it's " + orderedItem);
+        Debug.Log("Customer spawned at: " + spawnPoint.position);
+        Debug.Log("Customer active: " + newCustomer.activeSelf);
+        customerScript.SetOrder(orderedItem, SP);
+    }
+
+    public void CustomerServed()
+    {
+        UpdateMoney(orderedItem);
+        StartCoroutine(HandleCustomerServed());
+    }
+
+    private IEnumerator HandleCustomerServed()
+    {
+        yield return new WaitForSeconds(2f);
+        int spawnIndex = GetAvailableSpawnPointIndex();
+        if (spawnIndex != -1)
+        {
+            SpawnCustomer(spawnPoints[spawnIndex], spawnIndex);
+        }
+    }
+
+    public void CustomerRejected()
+    {
+        StartCoroutine(HandleCustomerRejected());
+    }
+
+    private IEnumerator HandleCustomerRejected()
+    {
+        yield return new WaitForSeconds(10f);
+        int spawnIndex = GetAvailableSpawnPointIndex();
+        if (spawnIndex != -1)
+        {
+            SpawnCustomer(spawnPoints[spawnIndex], spawnIndex);
+        }
+    }
 
     void UpdateMoneyText()
     {
         moneyText.text = "$ " + money;
     }
-
-    // void Start()
-    // {
-    //     InitializeMenu();
-    //     UpdateMoneyText();
-    //     StartRound();
-    // }
-
-    // void InitializeMenu()
-    // {
-    //     menuItems.Add("BeefBurger");
-    //     menuItems.Add("PorkBurger");
-    //     menuItems.Add("Salad");
-    // }
-
-    // void StartRound()
-    // {
-    //     StartCoroutine(SpawnCustomers());
-    // }
-
-    // IEnumerator SpawnCustomers()
-    // {
-    //     // int customersToSpawn = Mathf.Min(totalCustomers, 2);
-    //     for (int i = 0; i < totalCustomers; i--)
-    //     {
-    //         GameObject customer = Instantiate(customerPrefab, spawnPoint.position, Quaternion.identity);
-    //         customer.GetComponent<customer>().orderedItem = menuItems[Random.Range(0, menuItems.Count)];
-    //         yield return new WaitForSeconds(2f);
-    //     }
-    //     totalCustomers -= customersToSpawn;
-    //     if (totalCustomers > 0)
-    //     {
-    //         EndRound();
-    //     }
-    //     else
-    //     {
-    //         EndGame();
-    //     }
-    // }
-
-    // void EndRound()
-    // {
-    //     StartRound();
-    // }
-
-    // void EndGame()
-    // {
-    //     moneyText.text = "Earn $ " + money + "today!";
-    // }
 
     public void UpdateMoney(string orderedItem)
     {
@@ -78,7 +138,7 @@ public class gameManager : MonoBehaviour
         {
             earnings += 70;
         }
-        else if (orderedItem == "Salad")
+        else if (orderedItem == "salad")
         {
             earnings += 40; 
         }
